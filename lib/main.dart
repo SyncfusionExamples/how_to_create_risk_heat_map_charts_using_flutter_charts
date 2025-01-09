9ffc64ab58e03fc3515376d6e25ff7a573d02513
KompelliSravanSyncfusion <kompellisravankumark@syncfusion.com>
2025-01-09 16:50:25 +0530

    Updated the sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
+import 'dart:ui' as ui;
 
 void main() {
   runApp(const MyApp());
@@ -13,19 +14,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RiskHeatMapChart(),
+      home: RiskHeatMap(),
     );
   }
 }
 
-class RiskHeatMapChart extends StatefulWidget {
-  const RiskHeatMapChart({super.key});
+class RiskHeatMap extends StatefulWidget {
+  const RiskHeatMap({super.key});
 
   @override
-  RiskHeatMapChartState createState() => RiskHeatMapChartState();
+  RiskHeatMapState createState() => RiskHeatMapState();
 }
 
-class RiskHeatMapChartState extends State<RiskHeatMapChart> {
+class RiskHeatMapState extends State<RiskHeatMap> {
   List<_SP500ReturnData>? _heatMapData;
   TooltipBehavior? _tooltipBehavior;
 
@@ -34,44 +35,21 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
     _tooltipBehavior = TooltipBehavior(
       enable: true,
       tooltipPosition: TooltipPosition.pointer,
-      builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
-          int seriesIndex) {
-        _SP500ReturnData data = _heatMapData![pointIndex];
-        String label = '';
-        switch (seriesIndex) {
-          case 0:
-            label =
-                'Year: ${data.year.year}, 3M: ${data.threeMonthsAfterFirstRateCut}%';
-            break;
-          case 1:
-            label =
-                'Year: ${data.year.year}, 6M: ${data.sixMonthsAfterFirstRateCut}%';
-            break;
-          case 2:
-            label =
-                'Year: ${data.year.year}, 1Y: ${data.oneYearAfterFirstRateCut}%';
-            break;
-        }
-        return Container(
-          color: Colors.black87,
-          padding: const EdgeInsets.all(8.0),
-          child: Text(label, style: const TextStyle(color: Colors.white)),
-        );
-      },
+      format: 'point.x : point.y%',
     );
     _heatMapData = <_SP500ReturnData>[
-      _SP500ReturnData(DateTime(1973), 10, -10.2, -6.2, -36.0),
-      _SP500ReturnData(DateTime(1974), 10, -14.7, -15.3, 7.5),
-      _SP500ReturnData(DateTime(1980), 10, 15.0, 28.9, 30.3),
-      _SP500ReturnData(DateTime(1981), 10, -11.0, -7.9, -17.8),
-      _SP500ReturnData(DateTime(1982), 10, -4.8, 17.4, 36.5),
-      _SP500ReturnData(DateTime(1987), 10, 0.1, 1.7, 7.5),
-      _SP500ReturnData(DateTime(1989), 10, 7.4, 7.5, 11.9),
-      _SP500ReturnData(DateTime(1995), 10, 10, 5.1, 13.4),
-      _SP500ReturnData(DateTime(1998), 10, 17.2, 26.5, 27.3),
-      _SP500ReturnData(DateTime(2001), 10, -16.3, -12.4, -14.9),
-      _SP500ReturnData(DateTime(2007), 10, -4.4, -11.8, -27.2),
-      _SP500ReturnData(DateTime(2019), 10, 3.8, 13.3, 14.5),
+      _SP500ReturnData(DateTime(1973), -10.2, -6.2, -36.0),
+      _SP500ReturnData(DateTime(1974), -14.7, -15.3, 7.5),
+      _SP500ReturnData(DateTime(1980), 15.0, 28.9, 30.3),
+      _SP500ReturnData(DateTime(1981), -11.0, -7.9, -17.8),
+      _SP500ReturnData(DateTime(1982), -4.8, 17.4, 36.5),
+      _SP500ReturnData(DateTime(1987), 0.1, 1.7, 7.5),
+      _SP500ReturnData(DateTime(1989), 7.4, 7.5, 11.9),
+      _SP500ReturnData(DateTime(1995), 10, 5.1, 13.4),
+      _SP500ReturnData(DateTime(1998), 17.2, 26.5, 27.3),
+      _SP500ReturnData(DateTime(2001), -16.3, -12.4, -14.9),
+      _SP500ReturnData(DateTime(2007), -4.4, -11.8, -27.2),
+      _SP500ReturnData(DateTime(2019), 3.8, 13.3, 14.5),
     ];
     super.initState();
   }
@@ -105,14 +83,16 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildSP500Chart());
+    return Scaffold(
+        backgroundColor: Colors.blueGrey.shade900,
+        body: Padding(
+            padding: const EdgeInsets.all(20), child: _buildSP500Chart()));
   }
 
   SfCartesianChart _buildSP500Chart() {
     return SfCartesianChart(
       backgroundColor: Colors.blueGrey.shade900,
       plotAreaBorderWidth: 0,
-      isTransposed: true,
       title: const ChartTitle(
         text: 'S&P 500 Returns After Rate Cuts',
         textStyle: TextStyle(
@@ -123,15 +103,14 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
       ),
       primaryXAxis: DateTimeCategoryAxis(
         dateFormat: DateFormat.y(), // Format to display only the year
-        intervalType: DateTimeIntervalType.years,
         labelStyle: const TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 14.0,
           color: Colors.white,
         ),
-        majorTickLines: const MajorTickLines(size: 0),
         axisLine: const AxisLine(width: 0),
-        isInversed: true,
+        majorGridLines: const MajorGridLines(width: 0),
+        majorTickLines: const MajorTickLines(size: 0),
         title: const AxisTitle(
           text: 'Year of First Rate Cut',
           textStyle: TextStyle(
@@ -140,21 +119,31 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
             fontSize: 18.0,
           ),
         ),
-        majorGridLines: const MajorGridLines(width: 0),
       ),
       primaryYAxis: NumericAxis(
         opposedPosition: true,
         axisLine: const AxisLine(width: 0),
         majorGridLines: const MajorGridLines(width: 0),
-        edgeLabelPlacement: EdgeLabelPlacement.shift,
         majorTickLines: const MajorTickLines(size: 0),
-        borderWidth: 0,
-        labelIntersectAction: AxisLabelIntersectAction.multipleRows,
         labelStyle: const TextStyle(
           color: Colors.transparent, // Hide default labels
         ),
+        labelIntersectAction: AxisLabelIntersectAction.multipleRows,
         multiLevelLabelStyle: const MultiLevelLabelStyle(
             borderWidth: 0, borderColor: Colors.transparent),
+        multiLevelLabels: const <NumericMultiLevelLabel>[
+          NumericMultiLevelLabel(
+              start: -90,
+              end: -40,
+              text: 'Three months after \n first rate cut'),
+          NumericMultiLevelLabel(
+              start: -20, end: 20, text: 'Six months after \n first rate cut'),
+          NumericMultiLevelLabel(
+            start: 40,
+            end: 90,
+            text: 'One year after \n first rate cut',
+          ),
+        ],
         multiLevelLabelFormatter: (MultiLevelLabelRenderDetails details) {
           return ChartAxisLabel(
             details.text,
@@ -166,17 +155,6 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
             ),
           );
         },
-        multiLevelLabels: const <NumericMultiLevelLabel>[
-          NumericMultiLevelLabel(
-              start: 5, end: 30, text: '3 months after \n first rate cut'),
-          NumericMultiLevelLabel(
-              start: 40, end: 60, text: '6 months after \n first rate cut'),
-          NumericMultiLevelLabel(
-            start: 70,
-            end: 95,
-            text: '1 year after \n first rate cut',
-          ),
-        ],
       ),
       series: _getStackedColumnSeries(),
       tooltipBehavior: _tooltipBehavior,
@@ -185,88 +163,52 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
 
   List<CartesianSeries<_SP500ReturnData, DateTime>> _getStackedColumnSeries() {
     return <CartesianSeries<_SP500ReturnData, DateTime>>[
-      StackedColumn100Series<_SP500ReturnData, DateTime>(
+      StackedBar100Series<_SP500ReturnData, DateTime>(
         dataSource: _heatMapData!,
         xValueMapper: (_SP500ReturnData data, int index) => data.year,
         yValueMapper: (_SP500ReturnData data, int index) =>
-            data.returnValueIndicator,
+            data.threeMonthsAfterFirstRateCut,
         borderWidth: 5.0,
         borderRadius: const BorderRadius.all(Radius.circular(10)),
         width: 1,
         borderColor: Colors.blueGrey.shade900,
         pointColorMapper: (_SP500ReturnData data, int index) =>
             _buildColor(data.threeMonthsAfterFirstRateCut),
-        dataLabelSettings: DataLabelSettings(
-          isVisible: true,
-          builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
-              int seriesIndex) {
-            return Text(
-              '${data.threeMonthsAfterFirstRateCut.toString()}%',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-            );
-          },
-          labelAlignment: ChartDataLabelAlignment.middle,
-        ),
+        onCreateRenderer: (series) {
+          return _CustomHeatmapStackedBar100SeriesRenderer();
+        },
         name: '3 Months After First Rate Cut',
       ),
-      StackedColumn100Series<_SP500ReturnData, DateTime>(
+      StackedBar100Series<_SP500ReturnData, DateTime>(
         dataSource: _heatMapData!,
         xValueMapper: (_SP500ReturnData data, int index) => data.year,
         yValueMapper: (_SP500ReturnData data, int index) =>
-            data.returnValueIndicator,
+            data.sixMonthsAfterFirstRateCut,
         borderWidth: 5.0,
         borderRadius: const BorderRadius.all(Radius.circular(10)),
         width: 1,
         borderColor: Colors.blueGrey.shade900,
         pointColorMapper: (_SP500ReturnData data, int index) =>
             _buildColor(data.sixMonthsAfterFirstRateCut),
-        dataLabelSettings: DataLabelSettings(
-          isVisible: true,
-          builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
-              int seriesIndex) {
-            return Text(
-              '${data.sixMonthsAfterFirstRateCut.toString()}%',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-            );
-          },
-          labelAlignment: ChartDataLabelAlignment.middle,
-        ),
+        onCreateRenderer: (series) {
+          return _CustomHeatmapStackedBar100SeriesRenderer();
+        },
         name: '6 Months After First Rate Cut',
       ),
-      StackedColumn100Series<_SP500ReturnData, DateTime>(
+      StackedBar100Series<_SP500ReturnData, DateTime>(
         dataSource: _heatMapData!,
         xValueMapper: (_SP500ReturnData data, int index) => data.year,
         yValueMapper: (_SP500ReturnData data, int index) =>
-            data.returnValueIndicator,
+            data.oneYearAfterFirstRateCut,
         borderWidth: 5.0,
         borderRadius: const BorderRadius.all(Radius.circular(10)),
         width: 1,
         borderColor: Colors.blueGrey.shade900,
         pointColorMapper: (_SP500ReturnData data, int index) =>
             _buildColor(data.oneYearAfterFirstRateCut),
-        dataLabelSettings: DataLabelSettings(
-          isVisible: true,
-          builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
-              int seriesIndex) {
-            return Text(
-              '${data.oneYearAfterFirstRateCut.toString()}%',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-            );
-          },
-          labelAlignment: ChartDataLabelAlignment.middle,
-        ),
+        onCreateRenderer: (series) {
+          return _CustomHeatmapStackedBar100SeriesRenderer();
+        },
         name: '1 Year After First Rate Cut',
       ),
     ];
@@ -282,15 +224,82 @@ class RiskHeatMapChartState extends State<RiskHeatMapChart> {
 class _SP500ReturnData {
   _SP500ReturnData(
     this.year,
-    this.returnValueIndicator,
     this.threeMonthsAfterFirstRateCut,
     this.sixMonthsAfterFirstRateCut,
     this.oneYearAfterFirstRateCut,
   );
 
   final DateTime year;
-  final num returnValueIndicator;
   final num threeMonthsAfterFirstRateCut;
   final num sixMonthsAfterFirstRateCut;
   final num oneYearAfterFirstRateCut;
+}
+
+class _CustomHeatmapStackedBar100SeriesRenderer
+    extends StackedBar100SeriesRenderer<_SP500ReturnData, DateTime> {
+  _CustomHeatmapStackedBar100SeriesRenderer();
+
+  @override
+  StackedBar100Segment<_SP500ReturnData, DateTime> createSegment() {
+    return _CustomHeatmapStackedBar100Segment();
+  }
+}
+
+class _CustomHeatmapStackedBar100Segment
+    extends StackedBar100Segment<_SP500ReturnData, DateTime> {
+  _CustomHeatmapStackedBar100Segment();
+
+  @override
+  void transformValues() {
+    /// Given fixed y value and bottom value based on the y axis range.
+    if (series.index == 0) {
+      top = -33.33;
+      bottom = -100;
+    } else if (series.index == 1) {
+      top = 33.33;
+      bottom = -33.33;
+    } else {
+      top = 100;
+      bottom = 33.33;
+    }
+
+    super.transformValues();
+  }
+
+  @override
+  void onPaint(Canvas canvas) {
+    super.onPaint(canvas);
+
+    if (segmentRect == null) {
+      return;
+    }
+
+    final segment = series.dataSource![currentSegmentIndex];
+    num? yValue;
+    if (series.index == 0) {
+      yValue = segment.threeMonthsAfterFirstRateCut;
+    } else if (series.index == 1) {
+      yValue = segment.sixMonthsAfterFirstRateCut;
+    } else {
+      yValue = segment.oneYearAfterFirstRateCut;
+    }
+
+    final TextSpan textSpan = TextSpan(
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+      text: '${yValue.toString()}%',
+    );
+    final TextPainter textPainter = TextPainter(
+      text: textSpan,
+      textAlign: TextAlign.center,
+      textDirection: ui.TextDirection.ltr,
+    );
+    textPainter.layout();
+    double textX = segmentRect!.center.dx - (textPainter.width / 2);
+    double textY = segmentRect!.center.dy - (textPainter.height / 2);
+    textPainter.paint(canvas, Offset(textX, textY));
+  }
 }
